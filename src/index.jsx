f2964890c3b5bbe44fd582f88aa3ffaa4020fa6f
Fernando Brino <fernandobrino11@gmail.com>
2022-05-24 20:28:51 -0300

    basic setup

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import {render} from 'react-dom';
+import { App } from './App';
+
+// render - recebe dois parametros o primeiro o que eu quero renderizar (exibir em tela)
+// e o segundo é dentro de qual elemento eu quero renderizar essa informação
+render(<App/>, document.getElementById('root'))
