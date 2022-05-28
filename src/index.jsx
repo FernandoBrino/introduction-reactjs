65abc8362cd3dbd6ff619181c85462857bb0805e
Fernando Brino <fernandobrino11@gmail.com>
2022-05-28 15:27:49 -0300

    Fix deprecated use render

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
-import {render} from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import { App } from './App';
 
 // render - recebe dois parametros o primeiro o que eu quero renderizar (exibir em tela)
 // e o segundo é dentro de qual elemento eu quero renderizar essa informação
-render(<App/>, document.getElementById('root'))
+
+const container = document.getElementById('root')
+const root = createRoot(container);
+root.render(<App/>)
